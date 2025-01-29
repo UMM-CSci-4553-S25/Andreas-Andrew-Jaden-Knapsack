845215ff8f8c17e30eff1a301df2b07d33725a33
Andrew Lam <lam00139@morris.umn.edu>
2025-01-29 14:27:13 -0600

    implemented main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    // The returns the unit type `()` wrapped in the `Ok` variant of
+    // `Result`. The lack of a semicolon (`;`) at the end of the line
+    // makes this the last value in the function, which is what Rust
+    // will return in the absence of an explicit `return` statement.
+    Ok(())
 }
