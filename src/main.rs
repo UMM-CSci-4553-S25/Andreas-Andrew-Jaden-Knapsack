4b2bfd9b1eae9a93bc2d29b4706efabe2e5b15f1
Jaden Rainey <raine123@morris.umn.edu>
2025-01-31 14:35:37 -0600

    added dataset, main runs to instances and compares

--- src/main.rs
+++ src/main.rs
@@ -44,61 +44,65 @@ fn report_on_generation(
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut rng = rand::rng();
-    let file_path = "knapsacks/big.txt";
-    let knapsack = Knapsack::from_file_path(file_path)?;
+    let rng = std::cell::RefCell::new(rand::rng());
+    let file_path1 = "knapsacks/test.txt";
+    let file_path2 = "knapsacks/big.txt";
 
-    let mut best_in_run = None;
+    let knapsack1 = Knapsack::from_file_path(file_path1)?;
+    let knapsack2 = Knapsack::from_file_path(file_path2)?;
 
-    let run = Run::builder()
-        // The number of bits should equal the number of items.
-        .bit_length(knapsack.num_items())
-        // The maximum number of generations to run; this is somewhat arbitrary
+    let mut best_in_run1 = None;
+    let mut best_in_run2 = None;
+
+    let run1 = Run::builder()
+        .bit_length(knapsack1.num_items())
         .max_generations(1_000)
-        // The population size, which is also somewhat arbitrary, but larger is better
-        // until it's so big that memory management becomes a problem.
         .population_size(1_000)
-        // How do we want to select parent individuals? This takes two individuals at
-        // random from the population, and then chooses the better of the two from this
-        // tournament. You can change this to larger tournaments by changing `2` to your
-        // desired tournament size.
         .selector(Tournament::of_size::<2>())
-        // How do we want to mutate individual knapsack solutions? This flips
-        // on average one bit, thereby adding or removing one item from the solution.
         .mutator(WithOneOverLength)
-        // How do we want to recombine parent solutions? This randomly chooses for
-        // each bit whether to take it from the first or the second parent, giving
-        // use a "shuffled" set of choices from both parents.
         .recombinator(UniformXo)
-        // Do we want to use parallel evaluation? If this is `true`, the run will use
-        // all the available cores to evaluate the population in parallel. This can speed
-        // up the process considerably, at the cost of heating up your CPU.
         .parallel_evaluation(true)
-        // How do we want to score different knapsack "solutions"? This is the only
-        // problem dependent part of building the run. We'll start with a simple scorer
-        // that returns a `CliffScore`. This is an `enum` with two variants: `Score(v)`
-        // where `v` is the value of the items if they fit in the knapsack
-        // and `Overloaded` otherwise.  This is implemented so that `Overloaded` is
-        // always worse than any `Score(v)` value.
-        .scorer(CliffScorer::new(knapsack))
-        // Add an inspector. This is a function that is called after each generation
-        // and can be used to collect and/or print out information about the run. We'll use this to
-        // print out the best score in each generation, and to keep track of the best score in the run.
+        .scorer(CliffScorer::new(knapsack1))
         .inspector(|generation_number, population| {
-            report_on_generation(generation_number, population, &mut best_in_run, &mut rng);
+            report_on_generation(generation_number, population, &mut best_in_run1, &mut rng.borrow_mut());
         })
-        // Now that we've specified all the elements, we can build the run.
         .build();
 
-    let final_population = run.execute()?;
+    let run2 = Run::builder()
+        .bit_length(knapsack2.num_items())
+        .max_generations(1_000)
+        .population_size(1_000)
+        .selector(Tournament::of_size::<2>())
+        .mutator(WithOneOverLength)
+        .recombinator(UniformXo)
+        .parallel_evaluation(true)
+        .scorer(CliffScorer::new(knapsack2))
+        .inspector(|generation_number, population| {
+            report_on_generation(generation_number, population, &mut best_in_run2, &mut rng.borrow_mut());
+        })
+        .build();
 
-    let best = Best.select(&final_population, &mut rng)?;
-    println!("Best in final generation {best:?}");
-    println!("Best in overall run: {best_in_run:?}");
+    let final_population1 = run1.execute()?;
+    let final_population2 = run2.execute()?;
 
-    // The returns the unit type `()` wrapped in the `Ok` variant of
-    // `Result`. The lack of a semicolon (`;`) at the end of the line
-    // makes this the last value in the function, which is what Rust
-    // will return in the absence of an explicit `return` statement.
+    let best1 = Best.select(&final_population1, &mut rng.borrow_mut())?;
+    let best2 = Best.select(&final_population2, &mut rng.borrow_mut())?;
+
+    println!("Best in final generation for dataset 1: {best1:?}");
+    println!("Best in overall run for dataset 1: {best_in_run1:?}");
+    println!("Best in final generation for dataset 2: {best2:?}");
+    println!("Best in overall run for dataset 2: {best_in_run2:?}");
+
+    // Compare the results
+    if let (Some(b1), Some(b2)) = (best_in_run1, best_in_run2) {
+        if b1.test_results > b2.test_results {
+            println!("Dataset 1 has the better result.");
+        } else if b1.test_results < b2.test_results {
+            println!("Dataset 2 has the better result.");
+        } else {
+            println!("Both datasets have equal results.");
+        }
+    }
+
     Ok(())
 }
